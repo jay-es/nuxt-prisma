a87d2464197487e137e15598064dc79f54bea267
Jun Shindo <46585162+jay-es@users.noreply.github.com>
2024-04-04 21:08:10 +0900

    Prisma アップデート
    seed を createMany に変更

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -15,45 +15,32 @@ const prisma = new PrismaClient();
 async function main() {
   console.log("fetch");
   const data: Data = await fetch(
-    "https://raw.githubusercontent.com/typicode/jsonplaceholder/master/data.json"
+    "https://raw.githubusercontent.com/typicode/jsonplaceholder/master/data.json",
   ).then((res) => res.json());
 
   console.log("delete");
   await prisma.user.deleteMany();
 
   console.log("users");
-  // SQLite に createMany はない。トランザクションにしないと遅い
-  await prisma.$transaction(
+  await prisma.user.createMany({
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    data.users.map(({ address, company, ...data }) =>
-      prisma.user.create({ data })
-    )
-  );
+    data: data.users.map(({ address, company, ...user }) => user),
+  });
 
   console.log("posts");
-  await prisma.$transaction(
-    data.posts.map((post) => prisma.post.create({ data: post }))
-  );
+  await prisma.post.createMany({ data: data.posts });
 
   console.log("comments");
-  await prisma.$transaction(
-    data.comments.map((comment) => prisma.comment.create({ data: comment }))
-  );
+  await prisma.comment.createMany({ data: data.comments });
 
   console.log("todos");
-  await prisma.$transaction(
-    data.todos.map((todo) => prisma.todo.create({ data: todo }))
-  );
+  await prisma.todo.createMany({ data: data.todos });
 
   console.log("albums");
-  await prisma.$transaction(
-    data.albums.map((album) => prisma.album.create({ data: album }))
-  );
+  await prisma.album.createMany({ data: data.albums });
 
   console.log("photos");
-  await prisma.$transaction(
-    data.photos.map((photo) => prisma.photo.create({ data: photo }))
-  );
+  await prisma.photo.createMany({ data: data.photos });
 }
 
 main()
